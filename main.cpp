bfacd66e9a842f55c500036656e7ffdfd95929a9
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-03-10 17:19:07 +0200

    Cerințe tema 1
    - Compunere de clase
    - Constructori de inițializare
    - Rule of three (cc/op=/destr)
    - op<<

--- main.cpp
+++ main.cpp
@@ -2,9 +2,91 @@
 #include <array>
 
 #include <Helper.h>
+#include <ostream>
+#include <vector>
+
+class Student {
+    std::string nume;
+    int grupa;
+    bool chef;
+
+public:
+    Student(const std::string &nume, int grupa, bool chef)
+        : nume(nume),
+          grupa(grupa),
+          chef(chef) {
+        std::cout << "constr init student: " << nume << std::endl;
+    }
+
+    ~Student() {
+        std::cout << "destr student " << nume << std::endl;
+    }
+
+    Student(const Student &other)
+        : nume(other.nume),
+          grupa(other.grupa),
+          chef(other.chef) {
+        std::cout << "cc student: " << nume << std::endl;
+    }
+
+    Student& operator=(const Student &other) {
+        if (this == &other)
+            return *this;
+        nume = other.nume;
+        grupa = other.grupa;
+        chef = other.chef;
+        std::cout << "op= student: " << nume << std::endl;
+        return *this;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Student& student);
+};
+
+class Profesor {
+    std::string nume;
+    short salariu;
+
+public:
+    Profesor(const std::string &nume, short salariu)
+        : nume(nume),
+          salariu(salariu) {
+        std::cout << "constr init profesor: " << nume << std::endl;
+    }
+};
+
+class Facultate {
+    std::string nume;
+    std::vector<Student> studenti;
+    std::vector<Profesor> profi;
+
+public:
+    Facultate() = default;
+
+    explicit Facultate(const std::string &nume)
+        : nume(nume) {
+    }
+
+    Facultate(const std::string &nume, const std::vector<Student> &studenti, const std::vector<Profesor> &profi)
+        : nume(nume),
+          studenti(studenti),
+          profi(profi) {
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const Student& student) {
+    os << student.nume << " " << student.grupa << " " << student.chef << std::endl;
+    return os;
+}
 
 int main() {
+
+    Student st1{"abc", 131, true}, st2(st1), st3(st2);
+    //st1 = st2 = st3;
+    //st1.operator=(st2.operator=(st3));
     std::cout << "Ce frumos e afară când nu sunt bormașini!\n";
+    operator<<(std::cout, "Ce \"frumos\" e C++...\n");
+    // (std::cout << st1) << st2;
+    std::cout << st1 << st2;
+    operator<<(operator<<(std::cout, st1), st2);
     std::array<int, 100> v{};
     int nr;
     std::cout << "Introduceți nr: ";
